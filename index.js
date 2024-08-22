01ef387583835f1538d9d7cc59955ee49923c436
Shooroopoos <105969153+Shooroopoos@users.noreply.github.com>
2024-08-22 20:50:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+button = document.querySelector('button');
+output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Падла ты и ЗАСРАНЕЦ';
+});
